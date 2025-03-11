ae4f352d4d1849e8a28b5895b32fab6cbc47720c
Mateusz <115219630+Cent0l@users.noreply.github.com>
2025-03-11 14:26:40 +0100

    Update first.py

--- first.py
+++ first.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+#za pomoca wczytywania z pliku
 a=np.array([
     (1,1,1,1,3,1,1),
     (1,1,1,1,3,2,1),
@@ -11,7 +11,19 @@ a=np.array([
     (1,1,2,2,4,1,1)])
 
 
-for i in range(0,6):
-    for j in range(0,7):
-        print(a[i,j], end=" ")
-    print("\n")
+
+for i in range(0,8):#wiersze
+    for j in range(0,6):#kolumny
+        temp=a[i,j]
+        d=a[i,6]
+        if
+
+
+
+
+
+
+
+
+
+
